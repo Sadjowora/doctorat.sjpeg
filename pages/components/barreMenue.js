3fa70517f975d7af72c86f869ca21ca8482e95e8
Sadjo Wora Diallo <geniewora@gmail.com>
2023-05-09 08:17:42 +0100

    Realisation du squellette de l'application en place, allons pour le developpement pure du composant..

--- pages/components/barreMenue.js
+++ pages/components/barreMenue.js
@@ -1,56 +1,51 @@
 import Head from 'next/head';
 import styles from '../../styles/Menue.module.css';
 import Link from 'next/link';
-
 export default function Menue (){
 
 return (
-    <div className={styles.container}>
+    <div>
         <Head>
            <title> Noullal </title>
         </Head>
         <main>
-          <h1 className={styles.title}> Le Menue de Noullal </h1>
-        </main>
-        <Link href="/" className={styles.liens}> Home </Link>
-        <div className="dropdown m-3">
-            <button
-              className="btn btn-secondary dropdown-toggle"
-              type="button"
-              data-bs-toggle="dropdown"
-              id="dropdownMenuButton1"
-              aria-expanded="false"
-            >
-              Dropdown button
-            </button>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-              <li>
-                <a className="dropdown-item" href="#">
-                  Option 1
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Option 2
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Option 3
-                </a>
-              </li>
-            </ul>
-          </div>
-        <style jsx>{`
-          main {
-            padding: 5rem 0;
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-             }
-        `} </style>
+          <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top" style={{backgroundColor: '#ffd200'}}>
+            <div className="container-fluid d-flex justify-content-between">
+              <Link className="navbar-brand w-25" href="#">
+                <img src="#" alt="#" width="30" height="24" className="d-inline-block align-text-top"/> Noullal
+              </Link>
+              <button className="navbar-toggler w-25" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className={styles.menueCollapse}>
+                <div className="collapse navbar-collapse " id="navbarNav">
+                  <ul className="navbar-nav me-auto my-2 my-lg-0">
+                    <li className="nav-item">
+                      <a className="nav-link active" aria-current="page" href="#">Accueill</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Services</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Nous Contacter</a>
+                    </li>
+                  </ul>
+                 </div>
+                 <button className="btn btn-primary bg-dark"> Nous Contacter</button>
+                </div>
+            </div>
+         </nav>
+       </main>
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+           }
+      `} </style>
      </div>
 
   )
