51014984f84ceef15f031a57a91111d7e8b3d22f
Sadjo Wora Diallo <geniewora@gmail.com>
2023-07-27 04:46:51 +0100

    Maj, des cartes des formation donnees dans l'ecole, il ya plus qu'a attendre la validation du client..

--- pages/components/services.js
+++ pages/components/services.js
@@ -10,7 +10,7 @@ function Services () {
        <div className="d-flex justify-content-center"> <h1 style={{color: "#2e86c1"}}> Les formations que nous offrons  </h1> </div>
         <div className={"d-flex justify-content-between flex-wrap "+styles.axeVocation}>
 
-        <Link href="/science-juridique" className={styles.card}>
+        <Link href="/components/formations" className={styles.card}>
           <img src="/images/science-juridique.jpg" alt="abordable" />
           <div className="flex-column">
               <h3 className="mb-2"> Sciences Juridiques </h3>
@@ -18,7 +18,7 @@ function Services () {
           </div>
         </Link>
 
-        <Link href="/science-juridique" className={styles.card}>
+        <Link href="/components/formations" className={styles.card}>
           <img src="/images/science-po.jpg" alt="abordable" />
           <div className="flex-column">
               <h3 className="mb-2"> Sciences Politiques </h3>
@@ -26,7 +26,7 @@ function Services () {
           </div>
         </Link>
 
-        <Link href="/science-juridique" className={styles.card}>
+        <Link href="/components/formations" className={styles.card}>
           <img src="/images/science-economique.jpg" alt="image science-juridique" />
           <div className="flex-column">
               <h3 className="mb-2"> Sciences Economiques</h3>
@@ -34,7 +34,7 @@ function Services () {
           </div>
         </Link>
 
-        <Link href="/science-juridique" className={styles.card}>
+        <Link href="/components/formations" className={styles.card}>
           <img src="/images/science-gestion.webp" alt="science-gestion" />
           <div className="flex-column">
               <h3 className="mb-2"> Sciences de Gestion</h3>
